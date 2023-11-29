8067fd52877aa388ad1e1bd70174ced2b80a5d66
shihyao001 <shihyao001@ibears.io>
2023-11-29 11:07:46 +0800

    update code and add auto get league name

--- controller/currencyExchange.js
+++ controller/currencyExchange.js
@@ -1,50 +1,70 @@
 const fetch = require('node-fetch');
 require('dotenv/config')
 
-const getURLFromGGC = async searchJsonReady => {
+async function getLeagueNameFromGGC() {
     try {
+        const requestOption = {
+            method: 'GET',
+        };
+
+        const res = await fetch('https://web.poe.garena.tw/api/trade/data/leagues', requestOption);
+        const data = await res.json();
+
+        return data.result[0].id;
+    } catch (error) {
+        console.log(error);
+    }
+} 
+
+async function getURLFromGGC(searchJsonReady) {
+    try {
+        const leagueName = await getLeagueNameFromGGC();
         const requestOption = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 Accept: 'application/json',
                 'User-Agent': 'OAuth poe-bot/1.0.0 (contact: shihyao001@gmail.com)',
-                cookie: process.env.COOKIE,
             },
             body: JSON.stringify(searchJsonReady),
         }
-        // const delay = time => new Promise(resolve => setTimeout(resolve, time))
 
-        const res = await fetch('http://web.poe.garena.tw/api/trade/exchange/卡蘭德', requestOption) //TODO 每季要改成新聯盟 
-        // await delay(3000)
+        const res = await fetch(`http://web.poe.garena.tw/api/trade/exchange/${leagueName}`, requestOption)
         const data = await res.json()
 
-        return data
+        return { data, leagueName }
     } catch (error) {
         console.log(error);
     }
 }
 
-const divine = async (query) => {
-    const data = await getURLFromGGC(query);
-
-    const obj = data.result;
-
+async function divine(query) {
+    const URLResult = await getURLFromGGC(query);
+    const { data, leagueName } = URLResult;
+    const dataResultArray = Object.values(data.result)
     const searchURL = data.id;
+    const searchLength = dataResultArray.length < 12 ? dataResultArray.length : 12;
 
     const top3Value = [];
 
-    for (let i = 0; i < 12; i++) {
-        const D = Object.values(obj)[i].listing.offers.filter(el => el.exchange.amount === 1).map(el => el.item.amount);
-        top3Value.push(D);
+    for (let i = 0; i < searchLength; i++) {
+        const divineAmount = dataResultArray[i].listing.offers
+            .filter(el => el.exchange.amount === 1)
+            .map(el => el.item.amount);
+        top3Value.push(divineAmount);
     }
 
     const finalDivinePrice = top3Value.flat();
 
-    return [finalDivinePrice, searchURL];
+    return {
+        finalDivinePrice,
+        searchURL,
+        leagueName,
+    };
 }
 
 module.exports = {
     getURLFromGGC,
     divine,
+    getLeagueNameFromGGC,
 }
--- src/embed.js
+++ src/embed.js
@@ -1,34 +1,42 @@
 const { EmbedBuilder } = require('discord.js');
-const { divine } = require('../controller/currencyExchange');
+const {
+        divine,
+} = require('../controller/currencyExchange');
 const searchJsonReady = require('./divinePrice.json');
 
 module.exports.getEmbedFromExchange = async function getEmbedFromExchange() {
-    const exchange = await divine(searchJsonReady);
+        const exchange = await divine(searchJsonReady);
+
+        const { 
+                finalDivinePrice,
+                searchURL,
+                leagueName,
+        } = exchange;
 
     // test data avoid rate limit 
     // const exchange = [[1041,950,950,920,920,910,901,900,861,850],'nb3aQvH0'];
 
-    const formatExchange = exchange[0].map(e => e + 'c').toString();
+        const formatExchange = finalDivinePrice.map(divinePrice => divinePrice + 'c').toString();
 
-    return new EmbedBuilder()
-        .setDescription('目前1D換C的價格，取官網前10筆')
+        return new EmbedBuilder()
+        .setDescription(`目前1D換C的價格，取官網前${finalDivinePrice.length}筆`)
         .setTitle('官網連結')
-        .setURL(`https://web.poe.garena.tw/trade/exchange/卡蘭德/${exchange[1]}`)
+        .setURL(`https://web.poe.garena.tw/trade/exchange/${leagueName}/${searchURL}`)
         .addFields(
-            { name: '第 1 ~ 10 筆', value: formatExchange },
-            { name: '換算成c:', value: '取第二筆為基準作換算，小數點無條件捨去' },
-            { name: '\u200b', 
-            value: `0.1D = ${Math.trunc(exchange[0][1] * 0.1).toString() + 'c' } \n 
-                    0.2D = ${Math.trunc(exchange[0][1] * 0.2).toString() + 'c' } \n
-                    0.3D = ${Math.trunc(exchange[0][1] * 0.3).toString() + 'c' } \n 
-                    0.4D = ${Math.trunc(exchange[0][1] * 0.4).toString() + 'c' } \n 
-                    0.5D = ${Math.trunc(exchange[0][1] * 0.5).toString() + 'c' } \n `
-                    , inline: true},
-            { name: '\u200b', 
-            value: `0.6D = ${Math.trunc(exchange[0][1] * 0.6).toString() + 'c'} \n 
-                    0.7D = ${Math.trunc(exchange[0][1] * 0.7).toString() + 'c'} \n 
-                    0.8D = ${Math.trunc(exchange[0][1] * 0.8).toString() + 'c'} \n 
-                    0.9D = ${Math.trunc(exchange[0][1] * 0.9).toString() + 'c'} \n ` 
-                    , inline: true },
-            );
+                { name: `第 1 ~ ${finalDivinePrice.length} 筆`, value: formatExchange },
+                { name: '換算成c:', value: '取第二筆為基準作換算，小數點無條件捨去' },
+                { name: '\u200b', 
+                 value: `0.1D = ${Math.trunc(finalDivinePrice[1] * 0.1).toString() + 'c' } \n 
+                        0.2D = ${Math.trunc(finalDivinePrice[1] * 0.2).toString() + 'c' } \n
+                        0.3D = ${Math.trunc(finalDivinePrice[1] * 0.3).toString() + 'c' } \n 
+                        0.4D = ${Math.trunc(finalDivinePrice[1] * 0.4).toString() + 'c' } \n 
+                        0.5D = ${Math.trunc(finalDivinePrice[1] * 0.5).toString() + 'c' } \n `
+                        , inline: true},
+                { name: '\u200b', 
+                value: `0.6D = ${Math.trunc(finalDivinePrice[1] * 0.6).toString() + 'c'} \n 
+                        0.7D = ${Math.trunc(finalDivinePrice[1] * 0.7).toString() + 'c'} \n 
+                        0.8D = ${Math.trunc(finalDivinePrice[1] * 0.8).toString() + 'c'} \n 
+                        0.9D = ${Math.trunc(finalDivinePrice[1] * 0.9).toString() + 'c'} \n ` 
+                        , inline: true },
+                );
 }
