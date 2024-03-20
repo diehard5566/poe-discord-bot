ecfa2c6e9c1f8d329a856221c23597539267de5c
shihyao001 <shihyao001@ibears.io>
2024-03-20 14:44:02 +0800

    update feature able to parse unique

--- controller/timeless-jewel.js
+++ controller/timeless-jewel.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 const { getLeagueNameFromGGC } = require('./currencyExchange');
+const poedbTW = require('../src/poedb-tw.json');
 
 async function transitionTimelessJewelURL(query) {
     // const query = 'https://www.pathofexile.com/api/trade/search/Affliction/R9l9B84I7'
@@ -18,6 +19,11 @@ async function transitionTimelessJewelURL(query) {
         const dataFromGGG = await resFromGGG.json()
         delete dataFromGGG.id;
 
+        if (dataFromGGG.query.name) {
+            dataFromGGG.query.name = poedbTW.data.find(el => el.us === dataFromGGG.query.name).lang;
+            delete dataFromGGG.query.type;
+        }
+
         // 把國際服的quey轉成台服的query
         const leagueName = await getLeagueNameFromGGC();
         const requestOptionForTW = {
@@ -25,14 +31,23 @@ async function transitionTimelessJewelURL(query) {
             headers: {
                 'Content-Type': 'application/json',
                 Accept: 'application/json',
-                'User-Agent': 'OAuth poe-bot/1.0.0 (contact: shihyao001@gmail.com)',
+                Cookie: process.env.COOKIE,
             },
             body: JSON.stringify(dataFromGGG),
         }
 
         const resFromTW = await fetch(`https://pathofexile.tw/api/trade/search/${leagueName}`, requestOptionForTW)
         const dataFromTW = await resFromTW.json()
-        const searchURLForTW = `https://pathofexile.tw/trade/search/${leagueName}/${dataFromTW.id}`
+
+        if (dataFromTW.error) {
+            console.log(dataFromTW.error.message);
+        }
+
+        let searchURLForTW = `https://pathofexile.tw/trade/search/${leagueName}/${dataFromTW.id}`
+
+        if (dataFromTW.id === undefined) {
+            searchURLForTW = '有錯誤，請確定網址正確，並稍後重試(1 min)'
+        }
 
         return searchURLForTW;
     } catch (error) {
